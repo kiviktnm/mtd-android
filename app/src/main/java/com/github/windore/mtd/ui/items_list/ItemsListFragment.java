5b6e9b39af20bb7641bc1e4e2e20adb8cc1770d4
Windore <windore.wi@protonmail.com>
2022-07-18 15:56:07 +0300

    Added weekday selection

--- app/src/main/java/com/github/windore/mtd/ui/items_list/ItemsListFragment.java
+++ app/src/main/java/com/github/windore/mtd/ui/items_list/ItemsListFragment.java
@@ -4,28 +4,49 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.CalendarView;
+import android.widget.Spinner;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
+import com.github.windore.mtd.R;
 import com.github.windore.mtd.databinding.FragmentItemsListBinding;
 
-public class ItemsListFragment extends Fragment {
+import java.time.DayOfWeek;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.Calendar;
+
+public class ItemsListFragment extends Fragment implements AdapterView.OnItemSelectedListener {
 
     private FragmentItemsListBinding binding;
+    private ItemsListViewModel itemsListViewModel;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        ItemsListViewModel itemsListViewModel =
-                new ViewModelProvider(this).get(ItemsListViewModel.class);
-
+        itemsListViewModel = new ViewModelProvider(this).get(ItemsListViewModel.class);
         binding = FragmentItemsListBinding.inflate(inflater, container, false);
-        View root = binding.getRoot();
 
-        //final TextView textView = binding.textItemsList;
-        //itemsListViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
-        return root;
+        return binding.getRoot();
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        Spinner spinner = binding.spinnerWeekdaySelection;
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(
+                view.getContext(),
+                R.array.weekdays,
+                android.R.layout.simple_spinner_dropdown_item
+        );
+        spinner.setAdapter(adapter);
+        spinner.setSelection(DayOfWeek.from(LocalDate.now()).getValue() - 1);
+        spinner.setOnItemSelectedListener(this);
     }
 
     @Override
@@ -33,4 +54,12 @@ public class ItemsListFragment extends Fragment {
         super.onDestroyView();
         binding = null;
     }
+
+    @Override
+    public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> adapterView) {}
 }
--- app/src/main/java/com/github/windore/mtd/ui/items_list/ItemsListViewModel.java
+++ app/src/main/java/com/github/windore/mtd/ui/items_list/ItemsListViewModel.java
@@ -1,19 +1,11 @@
 package com.github.windore.mtd.ui.items_list;
 
-import androidx.lifecycle.LiveData;
-import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
 public class ItemsListViewModel extends ViewModel {
 
-    private final MutableLiveData<String> mText;
 
     public ItemsListViewModel() {
-        mText = new MutableLiveData<>();
-        mText.setValue("This is items fragment");
     }
 
-    public LiveData<String> getText() {
-        return mText;
-    }
 }
