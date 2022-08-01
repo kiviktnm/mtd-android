95f7d9aabc582910d46296bc9cb0f45c93018127
Windore <windore.wi@protonmail.com>
2022-08-02 00:08:48 +0300

    Add basic licenses activity

--- app/src/main/java/com/github/windore/mtd/MainActivity.java
+++ app/src/main/java/com/github/windore/mtd/MainActivity.java
@@ -1,16 +1,22 @@
 package com.github.windore.mtd;
 
 import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 
 import com.github.windore.mtd.databinding.ActivityMainBinding;
+import com.google.android.gms.oss.licenses.OssLicensesMenuActivity;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 import java.io.BufferedReader;
@@ -43,6 +49,14 @@ public class MainActivity extends AppCompatActivity {
 
         com.github.windore.mtd.databinding.ActivityMainBinding binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.main_toolbar);
+        setSupportActionBar(toolbar);
+
+        Button licensesBtn = (Button) findViewById(R.id.licenses_btn);
+        licensesBtn.setOnClickListener(view -> {
+            startActivity(new Intent(this, OssLicensesMenuActivity.class));
+        });
 
         BottomNavigationView navView = findViewById(R.id.nav_view);
         // Passing each menu ID as a set of Ids because each
