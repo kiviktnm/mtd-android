8b985c8ce9a35e2d91974b02d90dba9e1b2ba6ac
Windore <windore.wi@protonmail.com>
2022-08-02 01:12:40 +0300

    Remove unused imports

--- app/src/main/java/com/github/windore/mtd/MainActivity.java
+++ app/src/main/java/com/github/windore/mtd/MainActivity.java
@@ -4,9 +4,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
-import android.view.View;
 import android.widget.Button;
-import android.widget.ImageButton;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
@@ -16,7 +14,6 @@ import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 
 import com.github.windore.mtd.databinding.ActivityMainBinding;
-import com.google.android.gms.oss.licenses.OssLicensesMenuActivity;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 import java.io.BufferedReader;
